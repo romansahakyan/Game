204420a0d0bfb3c4b21aee965b785cbd78760a23
romansahakyan <31965242+romansahakyan@users.noreply.github.com>
2017-10-24 18:57:25 +0400

    Update web.js

--- web.js
+++ web.js
@@ -1,5 +1,23 @@
-const fs = require("fs");
-fs.readFile("Game/index.html","utf8",
- function(err, data) {
-   console.log(data);
- });
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'Game/index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
